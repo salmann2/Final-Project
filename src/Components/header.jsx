2135d1a813d9da389047a17ebe6191a5c54c7a61
salmann2-cpu <salmann4800@gmail.com>
2025-09-15 17:39:33 -0700

    Final Project

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -5,7 +5,7 @@ function Header () {
         <header>
             <div className="flex justify-between">
                 <h1 className="font-bold font-serif text-3xl text-[#ff7b00] px-8 py-5">Restaurant</h1>
-                <ul className="flex text-l gap-10 px-15 py-5 ml-5">
+                <ul className="flex text-xl gap-10 px-15 py-5 ml-5">
                     <li>Home</li>
                     <li>About</li>
                     <li>Menu</li>
